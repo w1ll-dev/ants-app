ef50d774bfbf84eb3cce682c070e9d80c07cebea
willgomes-sl <will.gomes@spartalabs.com.br>
2023-02-12 13:23:25 -0300

    feat(style): add default typography

---
+++ src/styles/typography.ts
@@ -0,0 +1,17 @@
+import styled, { css } from 'styled-components/native';
+
+export const ButtonLabel = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.default};
+  font-size: 20px;
+`;
+
+export const AntInfo = styled.Text<{ bold?: boolean }>`
+  font-family: ${({ theme }) => theme.fonts.default};
+  font-size: 12px;
+
+  ${({ bold }) =>
+    bold &&
+    css`
+      font-weight: bold;
+    `}
+`;
